852de1710b234a8565559c10ebf3afc790ac1075
cj123462 <cj123462@gmail.com>
2016-12-29 12:04:05 +0200

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/criminallintent/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.criminallintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
